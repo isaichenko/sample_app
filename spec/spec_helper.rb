9b627367f6a5385225bd17a4ac3bc61d84896d47
isaichenko <isaichenko.e@gmail.com>
2017-08-18 15:58:50 +0300

    Initialize repository

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,9 +25,8 @@ RSpec.configure do |config|
     #     # => "be bigger than 2 and smaller than 4"
     # ...rather than:
     #     # => "be bigger than 2"
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
-
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
@@ -37,12 +36,19 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+
+
   # This option will default to `:apply_to_host_groups` in RSpec 4 (and will
   # have no way to turn it off -- the option exists only for backwards
   # compatibility in RSpec 3). It causes shared context metadata to be
   # inherited by the metadata hash of host groups and examples, rather than
   # triggering implicit auto-inclusion in groups with matching metadata.
+  
+
+
   config.shared_context_metadata_behavior = :apply_to_host_groups
+
+
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
@@ -93,4 +99,6 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
 end
+
