2563bbd35e5bc2ad7f56d0376ac65200d97110dc
isaichenko <isaichenko.e@gmail.com>
2017-09-14 11:14:25 +0300

    Add account activation with sendgrid2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   host = 'peaceful-escarpment-65450.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sandgrid.net', 
+    :address        => 'smtp.sendgrid.net', 
     :port           => '587',
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
